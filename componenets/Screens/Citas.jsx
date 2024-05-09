c75dae3175fa112b9cfae44c36c89f9e16ed14c2
Ericucho31 <101685717+Ericucho31@users.noreply.github.com>
2024-05-09 12:29:17 -0700

    dataJson

--- componenets/Screens/Citas.jsx
+++ componenets/Screens/Citas.jsx
@@ -2,40 +2,24 @@ import React, { useState } from "react";
 import { StyleSheet, ScrollView, Text, View, Image } from "react-native";
 import themeComponent from "../Theme/themeComponent";
 import NewDateRequest from "../Cards/NewDateRequest";
+import CitasJson from "../../ddinamico.json"
 
 export default function Citas() {
+    console.log(CitasJson)
 
     return (
         <ScrollView>
             <View style={themeComponent.background.backgroundView}>
-                <NewDateRequest imageSource={'https://covalto.com/static/78498ccda70933a5f1e3edc3e40d3cbe/34aca/Hero_Mobile_Cuenta_Personas_V1_1_8046e424ea.webp'}
-                    name={'Juan Rodriguez Torres de la Cruz'}
-                    hour={'12:00'}
-                    date={'Abril 26'} />
+                {CitasJson.citas.map((item) => {
+                    return (<NewDateRequest imageSource={'https://covalto.com/static/78498ccda70933a5f1e3edc3e40d3cbe/34aca/Hero_Mobile_Cuenta_Personas_V1_1_8046e424ea.webp'}
+                        name={item.nombre}
+                        hour={item.hora}
+                        date={item.fecha} />
+                    )
+                })}
 
-                <NewDateRequest imageSource={'https://images.hola.com/imagenes/estar-bien/20221018219233/buenas-personas-caracteristicas/1-153-242/getty-chica-feliz-t.jpg?tx=w_680'}
-                    name={'Irene López Saenz'}
-                    hour={'16:00'}
-                    date={'Abril 30'} />
 
-                <NewDateRequest imageSource={'https://img.freepik.com/foto-gratis/chico-guapo-seguro-posando-contra-pared-blanca_176420-32936.jpg'}
-                    name={'Sabastián Garcia Carranza'}
-                    hour={'12:00'}
-                    date={'Abril 30'} />
-                <NewDateRequest imageSource={'https://pymstatic.com/5844/conversions/personas-emocionales-wide_webp.webp'}
-                    name={'Jessica Hurtado Sánchez'}
-                    hour={'14:00'}
-                    date={'Abril 30'} />
 
-                <NewDateRequest imageSource={'https://pymstatic.com/5844/conversions/personas-emocionales-wide_webp.webp'}
-                    name={'Jessica Hurtado Sánchez'}
-                    hour={'14:00'}
-                    date={'Abril 30'} />
-
-                <NewDateRequest imageSource={'https://pymstatic.com/5844/conversions/personas-emocionales-wide_webp.webp'}
-                    name={'Jessica Hurtado Sánchez'}
-                    hour={'14:00'}
-                    date={'Abril 30'} />
 
             </View>
 
