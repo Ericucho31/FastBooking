fbd1d9afae5282b9f93aad2303a50a2c85f05042
Ericucho31 <eric.gd.3108@gmail.com>
2024-05-14 09:25:40 -0700

    minorSyntaxisChanges

--- componenets/Screens/Citas.jsx
+++ componenets/Screens/Citas.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { StyleSheet, ScrollView, Text, View, Image } from "react-native";
 import themeComponent from "../Theme/themeComponent";
-import NewDateRequest from "../Cards/NewDateRequest";
-import CitasJson from "../../dinamico.json"
 import DisplayNewDateRequests from "../Handler/DisplayNewDateRequest";
 
 export default function Citas() {
--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { BackHandler, Alert } from 'react-native';
 
 // Screens
 import Citas from '../componenets/Screens/Citas';
 import Calendario from '../componenets/Screens/Calendario';
 import PruebaScreen from '../componenets/Screens/Pruebas';
 import SettingsScreen from '../componenets/Screens/Setting';
-import Notification from '../componenets/Cards/NotificationCard';
 import NotififcationsScreen from '../componenets/Screens/Notifications';
+import { useDataContext } from '../componenets/Context/GlobalStateContext';
 
 //Screen names
 const datesName = "Citas";
@@ -18,15 +18,32 @@ const pruebaName = 'Prueba';
 const notificationsName = 'Notificaciones';
 const settingsName = 'Ajustes';
 
-
 const Tab = createBottomTabNavigator();
 
 function MainContainer({ navigation }) {
+
+  const {state, dispatch} = useDataContext()
+
+  useEffect(() => {
+    const backHandler = navigation.addListener('beforeRemove', (e) => {
+      if (!navigation.canGoBack()) {
+        // If the user cannot go back, exit the app
+        BackHandler.exitApp();
+        return;
+      }
+      else {
+        // If the user can go back, go back to the previous screen
+        BackHandler.exitApp();
+      }
+    });
+
+    
+  }, [navigation]);
+
   return (
 
     <Tab.Navigator
       initialRouteName={datesName}
-
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
@@ -63,15 +80,13 @@ function MainContainer({ navigation }) {
         style: { padding: 10, height: 70 }
       }}>
 
-      <Tab.Screen name={datesName} component={Citas} />
+      <Tab.Screen name={datesName} component={Citas} options={{tabBarBadge: state.citasPendientes.length !== 0 ? state.citasPendientes.length : null}}/>
       <Tab.Screen name={calendarName} component={Calendario} />
       <Tab.Screen name={notificationsName} component={NotififcationsScreen} />
       <Tab.Screen name={settingsName} component={SettingsScreen} />
       <Tab.Screen name={pruebaName} component={PruebaScreen} />
 
-
     </Tab.Navigator>
-
   );
 }
 
